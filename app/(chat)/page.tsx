df639ef42e966af78acd06c7451837893a984338
ProkopiyEgorov <a9124780@gmail.com>
2025-04-05 19:22:42 +1000

    Update page.tsx

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -8,32 +8,13 @@ import { DataStreamHandler } from '@/components/data-stream-handler';
 export default async function Page() {
   const id = generateUUID();
 
-  const cookieStore = await cookies();
-  const modelIdFromCookie = cookieStore.get('chat-model');
-
-  if (!modelIdFromCookie) {
-    return (
-      <>
-        <Chat
-          key={id}
-          id={id}
-          initialMessages={[]}
-          selectedChatModel={DEFAULT_CHAT_MODEL}
-          selectedVisibilityType="private"
-          isReadonly={false}
-        />
-        <DataStreamHandler id={id} />
-      </>
-    );
-  }
-
   return (
     <>
       <Chat
         key={id}
         id={id}
         initialMessages={[]}
-        selectedChatModel={modelIdFromCookie.value}
+        selectedChatModel={DEFAULT_CHAT_MODEL}
         selectedVisibilityType="private"
         isReadonly={false}
       />
